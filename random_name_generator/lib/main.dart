4022fa29364f6b9d7b8ffa0217dd2492532b1457
Chirag-Mathur <chiragmathurindia@gmail.com>
2021-02-03 23:17:37 +0530

    Adding Week 1 Part 2

--- random_name_generator/lib/main.dart
+++ random_name_generator/lib/main.dart
@@ -13,30 +13,78 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final wordpair = WordPair.random();
   final List<WordPair> _randomWordslist = <WordPair>[];
+  final Set<WordPair> _likedWords = {};
 
   Widget _randomTile(WordPair pair, int i) {
+    var _isLiked = _likedWords.contains(pair);
     return Card(
       child: ListTile(
         title: Text(pair.asPascalCase),
         leading: Text(i.toString()),
+        trailing: IconButton(
+          icon: Icon(
+            _isLiked ? Icons.favorite : Icons.favorite_outline,
+            color: _isLiked ? Colors.red : Colors.black,
+          ),
+          onPressed: () {
+            setState(() {
+              _isLiked = !_isLiked;
+            });
+            if (_isLiked) {
+              _likedWords.add(pair);
+            } else {
+              _likedWords.remove(pair);
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  void _likedList() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Liked Names'),
+            ),
+            body: ListView.builder(
+              itemCount: _likedWords.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(_likedWords.elementAt(index).asPascalCase),
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-   // _randomWordslist.addAll(generateWordPairs().take(10));
-    return ListView.builder(
-        //itemCount: _randomWordslist.length,
-        itemBuilder: (context, index) {
-          if(index>=_randomWordslist.length)
-          {
-            _randomWordslist.addAll(generateWordPairs().take(10));
-          }
-      
+    // _randomWordslist.addAll(generateWordPairs().take(10));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Random Word Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _likedList),
+        ],
+      ),
+      body: ListView.builder(
+          //itemCount: _randomWordslist.length,
+          itemBuilder: (context, index) {
+        if (index >= _randomWordslist.length) {
+          _randomWordslist.addAll(generateWordPairs().take(10));
+        }
 
-      return _randomTile(_randomWordslist[index], index);
-    });
+        return _randomTile(_randomWordslist[index], index);
+      }),
+    );
   }
 }
 
@@ -46,15 +94,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Random Word Generator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Random Word Generator')),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
